fc35eada83c27d841b161a9f49cc022be2c1e303
Maria <marycarol.00@gmail.com>
2024-01-13 17:45:02 -0300

    first commit

---
+++ src/components/Table.tsx
@@ -0,0 +1,14 @@
+function StarTable () {
+  return (
+    <table>
+    <thead>
+      <tr></tr>
+    </thead>
+    <tbody>
+      <tr></tr>
+    </tbody>
+    </table>
+  )
+};
+
+export default StarTable;
---
+++ src/context/context.tsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const StarContext = createContext({} as any);
+
+export default StarContext;
